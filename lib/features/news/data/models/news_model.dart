052095585e6921a157891c44f3879fef5d70e174
hasaneh299-spec <hasaneh299@gmail.com>
2026-07-24 17:47:59 +0100

    Create NewsModel class for news data structure

---
+++ lib/features/news/data/models/news_model.dart
@@ -0,0 +1,15 @@
+class NewsModel {
+  final String id;
+  final String title;
+  final String description;
+  final String imageUrl;
+  final String time;
+
+  NewsModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.time,
+  });
+}
