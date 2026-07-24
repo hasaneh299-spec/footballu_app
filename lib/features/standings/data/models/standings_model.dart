85432c5abf29820bf4e9a86009b775f2b1d20b54
hasaneh299-spec <hasaneh299@gmail.com>
2026-07-24 18:02:52 +0100

    Add StandingsModel class for team standings data

---
+++ lib/features/standings/data/models/standings_model.dart
@@ -0,0 +1,13 @@
+class StandingsModel {
+  final int rank;
+  final String teamName;
+  final int played;
+  final int points;
+
+  StandingsModel({
+    required this.rank,
+    required this.teamName,
+    required this.played,
+    required this.points,
+  });
+}
